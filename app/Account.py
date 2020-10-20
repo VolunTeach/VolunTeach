4b6261a468ad3e10b8cf26f4913c0e08c8c7ffae
Vivian Li <vivian.s.li@vanderbilt.edu>
2020-10-20 18:04:36 -0500

    incomplete Account class

---
+++ app/Account.py
@@ -0,0 +1,21 @@
+class Account:
+    def __init__(self):
+        self.name = ""
+        self.email = ""
+        self.avail = [[0 for j in range(48)] for i in range(7)]
+
+    def __init__(self, name, email, avail):
+        self.name = name
+        self.email = email
+        self.avail = avail
+
+    def setName(self, name):
+        self.name = name
+
+    def setEmail(self, email):
+        self.email = email
+
+    def setAvail(self, avail):
+        self.avail = avail
+
+    
