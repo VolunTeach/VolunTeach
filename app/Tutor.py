670d0565a5e49d94f7b5244de124ba384c1a4230
ngpatil <npatil314@gmail.com>
2020-10-21 22:10:08 -0500

    fixed default constructor on tutor.py

---
+++ app/Tutor.py
@@ -0,0 +1,37 @@
+#VolunTeach Tutoring Services
+
+#this class inherits Account
+class Tutor(Account):
+
+    #constructors
+    def __init__(self):
+        super().__init__(self)
+        self.subjects = ""
+        self.clients = ""
+        maxHours = 0
+
+    def __init__(self, name, email, avail, subjects, clients, maxHours):
+        super().__init__(self, name, email, avail)
+        self.subjects = subjects
+        self.clients = clients
+        self.maxHours = maxHours
+
+    #setters
+    def setSubjects(self, subjects):
+        self.subjects = subjects
+    
+    def setClients(self, clients):
+        self.clients = clients
+        
+    def setMaxHours(self, maxHours):
+        self.maxHours = maxHours
+
+    #getters
+    def getSubjects(self):
+        return self.subjects
+
+    def getClients(self):
+        return self.clients
+
+    def getMaxHours(self):
+        return self.maxHours
